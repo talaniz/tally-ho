84d508d038ee7abff03e38ff3cd610a14f01455e
Antonio Alaniz <antonio.alaniz@gmail.com>
2020-09-08 09:19:43 -0700

    Process get categories from cli

--- tally_ho/cmd.py
+++ tally_ho/cmd.py
@@ -11,6 +11,8 @@ def execute_category_action(cmd):
     """Execute a category based method."""
     if cmd.action == "create":
         return cmd.tally_ho.create_category(cmd.name)
+    elif cmd.action == None and cmd.name == None and cmd.quantity == None:
+        return cmd.tally_ho.get_categories()
 
 
 def process_cli_cmds(cmd):
--- tests/test_cmd.py
+++ tests/test_cmd.py
@@ -20,3 +20,18 @@ class TestCLI(unittest.TestCase):
         self.assertEqual(category.id, 1)
         self.assertEqual(category.name, "bugs")
 
+    def test_cli_gets_category(self):
+        th = tally_ho.TallyHo('test.db')
+        create_cat_cmd = cmd.Command("category", "create", "bugs", None, th)
+        category = cmd.process_cli_cmds(create_cat_cmd)
+
+        self.assertEqual(category.id, 1)
+        self.assertEqual(category.name, "bugs")
+
+        th = tally_ho.TallyHo('test.db')
+        get_cat_cmd = cmd.Command("category", None, None, None, th)
+        cat_list = cmd.process_cli_cmds(get_cat_cmd)
+        first_cat = cat_list[0]
+
+        self.assertEqual(len(cat_list), 1)
+        self.assertEqual(first_cat.name, "bugs")
